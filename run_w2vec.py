55ebb1f1cc1050b75a9cfddbb179698b98432f98
Snorre Ralund <snorre87@users.noreply.github.com>
2021-05-25 11:32:00 +0200

    w2vec helper script

---
+++ run_w2vec.py
@@ -0,0 +1,55 @@
+import nltk
+import gensim
+nltk.download('punkt')
+from gensim.models.phrases import Phrases, ENGLISH_CONNECTOR_WORDS
+import logging
+
+from gensim.models.word2vec import Word2Vec
+from collections import Counter
+def run_w2vec(texts,emb_size=64):
+
+    if type(texts[0])==str:
+        docs = [nltk.word_tokenize(i.lower()) for i in df.text]
+        print('Tokenizing...')
+    else:
+        docs = texts
+    print('Locating collocations...')
+    phrase_model_bi = Phrases(docs,connector_words=ENGLISH_CONNECTOR_WORDS)
+
+    phrase_docs_bi = [phrase_model_bi[sent] for sent in docs]
+    phrase_model = Phrases(phrase_docs_bi, delimiter=' ')
+    phrase_docs = [phrase_model[sent] for sent in phrase_docs_bi]
+    # train w2v
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    ############
+    ## missing
+    #####load pretrained and just retrain
+    ###############
+    ### Define parameters for the model ###
+    max_words = 100000
+    size=emb_size # Size of Embedding.
+    workers=4 # Number CPU Cores to use for training in Parallel.
+    iter_= 50 # Depending on the size of your data you might want to run through your data more than once.
+    window=6 # How much Context
+    min_count=5 # Number of Occurrences to be kept in the Vocabulary
+    count = 0
+    ws = Counter()
+    new_docs = docs+phrase_docs_bi+phrase_docs
+    for i in new_docs:
+        count+=len(i)
+        for w in i:
+            ws[w]+=1
+    print('%d unique words in corpus and %d count'%(len(ws),count))
+    ws = dict(ws.most_common(max_words))
+    min_words = 2000000
+    iter_ = min_words//count
+    iter_ = max(iter_,5)
+    w2v = Word2Vec(vector_size=size,workers=workers,negative=10,window=window,min_count=min_count) # max_final_vocab = max_words
+    w2v.build_vocab_from_freq(ws)
+    print('Getting ready to train')
+    #print(w2v.corpus_count)
+    new_docs = docs+phrase_docs_bi+phrase_docs
+    import random
+    random.shuffle(new_docs)
+    w2v.train(new_docs,total_words = count,epochs=iter_)
+    return w2v
