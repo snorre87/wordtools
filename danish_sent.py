b62505ca4934cf7cea01b4f08cf0668d7e705e4b
Snorre Ralund <snorre87@users.noreply.github.com>
2021-09-08 10:19:19 +0200

    update sent

--- danish_sent.py
+++ danish_sent.py
@@ -56,16 +56,18 @@ class DANISH_SENTIMENT():
     else:
       self.hisia=False
   def get_bert(self,text,clf,kwargs={}):
-
     res = clf.predict_proba(text, **kwargs)
+    d_s = {}
     classes = clf._classes()
     d = {}
     for num in range(len(classes)):
         em = res[num]
         cl = classes[num]
+        best = em.argmax()
+        d_s[num] = cl[best]
         for i,j in zip(cl,em):
           d[i] = j
-    return d
+    return d,d_s
   def get_sentiment(self,text,timings=False):
     d = {}
     if text.strip()=='':
@@ -75,17 +77,28 @@ class DANISH_SENTIMENT():
     lambda x: self.get_bert(x,clf=self.classifier,kwargs={'no_emotion':True}),
     lambda text: self.get_bert(text,clf=self.classifier_tone),
         lambda text: self.hisia(text).sentiment.positive_probability,
-        lambda text: self.nlp(text).cats.items()]
+        lambda text: self.nlp(text).cats]
     names = ['afinn','sentida','bert_emotion','bert_tone','hisia_posprob','spacy_sent']
     d_t = {}
+    d_s = {}
     for func,name in zip(funcs,names):
         try:
             t = time.time()
-            d[name] = func(text)
+            out = func(text)
+            if type(out)!=tuple:
+                d[name] = out
+                if type(out)==dict:
+                    d_s[name] = max(out,key=lambda x:x[1])[0]
+                else:
+                    d_s[name] = out
+            else:
+                d[name] = out[0]
+                d_s[name] = out[1]
+
             dt = time.time()-t
             d_t[name] = dt
         except Exception as e:
             pass
     if timings:
-      return d,d_t
-    return d
+      return d,d_s,d_t
+    return d,d_s
