25a817382b09c04468fcdf6c2beb7984d4bb0835
Snorre Ralund <snorre87@users.noreply.github.com>
2022-04-15 10:21:48 +0200

    Update topicality.py

--- topicality.py
+++ topicality.py
@@ -154,6 +154,7 @@ class Topicality():
             if ents_normed[i]>topicality_cut:
                 break
             if not wi in self.w2vec_w2id:
+                print(wi)
                 continue
             nodes.add(wi)
             if remove_duplicate_phrases:
@@ -188,6 +189,7 @@ class Topicality():
                     dup_g.add_edge(i,wi)
 
         #nodes = set([Index[i] for i in sort[0:nwords] if ps[i]<freq_cut and ents_normed[i]<topicality_cut])
+        print(len(nodes))
         if add_k_neighbors>0:
             # locate meaningful cut
             sort = ents_normed.argsort()
