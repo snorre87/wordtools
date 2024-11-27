cdd872e0f04440b7e11ac0feb99b85a71d404ae5
Snorre Ralund <au278937@uni.au.dk>
2024-11-27 12:21:47 +0100

    Update wordsim_networks.py

--- wordsim_networks.py
+++ wordsim_networks.py
@@ -552,7 +552,7 @@ def trim_counter(c,max_tokens,p=0.75):
     return c
 from tqdm import tqdm_notebook as tqdm
 
-def prepare_docs(docs,clean=lambda x:x,filter_func=lambda x: not x,stem=False,resolve_entities=True,return_e2e=False,phrases=False,run_in_memory=True,max_tokens=150000,verbose=False,index_files=True,index_folder='temp_indexed/',max_lans=3,trans_lans = {'sv':'da','no':'da'},detector=False):
+def prepare_docs(docs,clean=lambda x:x,filter_func=lambda x: not x,stem=False,resolve_entities=True,return_e2e=False,phrases=False,run_in_memory=True,max_tokens=150000,verbose=False,index_files=False,index_folder='temp_indexed/',max_lans=3,trans_lans = {'sv':'da','no':'da'},detector=False):
     """Function for preparing documents.
     Documents can be either a lists of strings, lists of tokenized docs or a path to a file for streaming data (documents should be separated by '\n\r').
     Tokenization, Cleaning, Mapping between original and cleaned version to merge entities, and Phrasing using collocation detector.
